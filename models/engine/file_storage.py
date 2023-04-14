efd26eb5c23430a3591c798d41c632e220be38f0
Túlio Benedito Nhantumbo <80028633+beneditotulio@users.noreply.github.com>
2023-04-14 18:37:57 +0200

    5. Delete object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -57,7 +57,7 @@ class FileStorage:
             json.dump(my_dict, f)
 
     def reload(self):
-        """serialize the file path to JSON file path
+        """serialize the file path to JSON file path 
         """
         try:
             with open(self.__file_path, 'r', encoding="UTF-8") as f:
